1cd0beeeb57acf3b34c9ecb9c545dbcb204226c5
Shiva Saini <shivrajsaini1052@gmail.com>
2026-07-10 14:15:10 +0530

    Add Theme change Button in navbar

--- public/js/script.js
+++ public/js/script.js
@@ -15,4 +15,27 @@
             form.classList.add('was-validated')
         }, false)
     })
-})()
+})();
+
+
+//  for theme change 
+
+const themeBtn = document.getElementById("theme-toggle");
+const themeIcon = themeBtn.querySelector("i");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    themeIcon.classList.replace("fa-moon", "fa-sun");
+}
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        themeIcon.classList.replace("fa-moon", "fa-sun");
+    } else {
+        localStorage.setItem("theme", "light");
+        themeIcon.classList.replace("fa-sun", "fa-moon");
+    }
+});
